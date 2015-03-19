0ded984dbf790bea725b836913b8cedd921df3ca
Alexey Veretennikov <alexey.veretennikov@gmail.com>
2015-03-19 11:03:23 +0100

    fixed warning for older bison

--- libkv.c
+++ libkv.c
@@ -7,6 +7,9 @@
 /* system includes */
 #include <assert.h>
 
+/* older bison not generates yyparse in header, so declare it here */
+int yyparse (struct kv_table_t* table);
+
 struct kv_table_t* libkv_parse_file(FILE* file)
 {
   struct kv_table_t* table = kv_table_alloc();
